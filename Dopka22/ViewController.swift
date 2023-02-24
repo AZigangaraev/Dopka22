24f890702291b3653ee656bb95d39781904c23f9
Amir Zigangaraev <amir.zigangaraev@gmail.com>
2023-02-24 15:32:01 +0300

    Initial Commit

---
+++ Dopka22/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dopka22
+//
+//  Created by Teacher on 24.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
